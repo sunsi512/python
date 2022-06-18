00c197c2690ef08fb1b3e5577a44c4c1cfa2b538
sunsi512 <d58230608@gmail.com>
2022-06-18 13:01:58 +0900

    .

--- py50.py
+++ py50.py
@@ -1,26 +1,26 @@
-class 자동차:
-    def __init__(self, 차주인, 차종):
-        self.name = 차주인
-        self.car = 차종
-    def 차정보(self):
-        print('{} 차의 주인은 {} 입니다.'.format(self.car, self.name))
-    def 차변경(self, 차종):
-        self.car = 차종
-    def __del__(self):
-        print('{}차 안내 종료'.format(self.name))
+# class 자동차:
+#     def __init__(self, 차주인, 차종):
+#         self.name = 차주인
+#         self.car = 차종
+#     def 차정보(self):
+#         print('{} 차의 주인은 {} 입니다.'.format(self.car, self.name))
+#     def 차변경(self, 차종):
+#         self.car = 차종
+#     def __del__(self):
+#         print('{}차 안내 종료'.format(self.name))
 
-# 클래스 사용
-# 객체화(클래스 사용을 위해 변수로 만들어줌)
-아빠차 = 자동차('아빠', '벤츠')     #클래스 사용 1
-엄마차 = 자동차('엄마', '아반떼')   #클래스 사용 2
-내차 = 자동차('나', 'k5')          #클래스 사용 3
+# # 클래스 사용
+# # 객체화(클래스 사용을 위해 변수로 만들어줌)
+# 아빠차 = 자동차('아빠', '벤츠')     #클래스 사용 1
+# 엄마차 = 자동차('엄마', '아반떼')   #클래스 사용 2
+# 내차 = 자동차('나', 'k5')          #클래스 사용 3
 
-아빠차.차정보()
-내차.차정보()
-엄마차.차정보()
-del 아빠차         # 아빠차 변수를 강제로 없앰
-엄마차. 차변경('제네시스')
-엄마차. 차정보()
+# 아빠차.차정보()
+# 내차.차정보()
+# 엄마차.차정보()
+# del 아빠차         # 아빠차 변수를 강제로 없앰
+# 엄마차. 차변경('제네시스')
+# 엄마차. 차정보()
 
 
 population = 0
@@ -37,7 +37,8 @@ class Person:
 
     def __del__(self):
         global population      # 클래스보다 위에 있는 전역변수를 수정할 때
-        population += 1
+        population -= 1
+        print('{} is dead'.format(self.name))
 
 man = Person('james')
 woman = Person('emliy')
